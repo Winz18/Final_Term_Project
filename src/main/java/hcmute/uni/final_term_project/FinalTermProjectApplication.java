9a34852b623d44d0a6120d78ab1538091dc2a090
Winz18 <117364628+Winz18@users.noreply.github.com>
2024-10-21 15:35:48 +0700

    Initial commit

---
+++ src/main/java/hcmute/uni/final_term_project/FinalTermProjectApplication.java
@@ -0,0 +1,13 @@
+package hcmute.uni.final_term_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalTermProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalTermProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hcmute/uni/final_term_project/FinalTermProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package hcmute.uni.final_term_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalTermProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
