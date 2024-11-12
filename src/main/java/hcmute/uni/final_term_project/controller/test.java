9743d2d9480dc816d467ba9ed960abaa36f5fc3e
Winz18 <117364628+Winz18@users.noreply.github.com>
2024-11-12 09:47:38 +0700

    up UI 002

---
+++ src/main/java/hcmute/uni/final_term_project/controller/test.java
@@ -0,0 +1,18 @@
+package hcmute.uni.final_term_project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class test {
+    @Controller
+    public static class MainController {
+
+        @GetMapping("/test_UI")
+        public String home(Model model) {
+            model.addAttribute("userName", "Nguyen Thang Loi");  // Truyền dữ liệu cho giao diện
+            return "user_template_test";
+        }
+    }
+
+}
