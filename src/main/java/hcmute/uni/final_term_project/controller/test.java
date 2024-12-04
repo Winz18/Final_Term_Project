4b66546729c5c8069a6e2fb04499306e7cfce7ba
Winz18 <117364628+Winz18@users.noreply.github.com>
2024-12-04 19:01:49 +0700

    ui 041224-01

--- src/main/java/hcmute/uni/final_term_project/controller/test.java
+++ src/main/java/hcmute/uni/final_term_project/controller/test.java
@@ -11,7 +11,7 @@ public class test {
         @GetMapping("/test_UI")
         public String home(Model model) {
             model.addAttribute("userName", "Nguyen Thang Loi");  // Truyền dữ liệu cho giao diện
-            return "user_template_test";
+            return "my-documents";
         }
     }
 
